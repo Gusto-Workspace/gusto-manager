5557ad4d3c9de456ed10139df6d9abb0bcdfba2e
Léo Baccialone <baccialone.leo@gmail.com>
2024-10-18 17:43:19 +0200

    wip dashbord restaurant

---
+++ client/src/pages/dishes.page.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+// I18N
+import { i18n } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+// COMPONENTS
+import MainHomeComponent from "@/components/home/main.home.component";
+
+export default function DishesPage(props) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      router.push("/login");
+    } else {
+      setLoading(false);
+    }
+  }, [router]);
+
+  let title;
+  let description;
+
+  switch (i18n.language) {
+    case "en":
+      title = "Gusto Manager";
+      description = "";
+      break;
+    default:
+      title = "Gusto Manager";
+      description = "";
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+
+        {/* <>
+          {description && <meta name="description" content={description} />}
+          {title && <meta property="og:title" content={title} />}
+          {description && (
+            <meta property="og:description" content={description} />
+          )}
+          <meta
+            property="og:url"
+            content="https://lespetitsbilingues-newham.com/"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="/img/open-graph.jpg" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+        </> */}
+      </Head>
+
+      <div>
+        {loading ? (
+          <div className="flex justify-center items-center h-screen">
+            <div className="loader">Loading...</div>
+          </div>
+        ) : (
+          <div> </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "dishes"])),
+    },
+  };
+}
