9049b40b5b698da3826bee9092a2e28b6ac53c34
Gusto Manager <gusto.workspace@gmail.com>
2026-01-27 19:36:49 +0100

    update UX UI reservations

--- client/src/pages/_document.page.js
+++ client/src/pages/_document.page.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+
         <link rel="icon" href="/favicon.ico?v=2" type="image/x-icon" />
         {/*<link rel="stylesheet" href="https://use.typekit.net/hqy2xcj.css" />*/}
         <link
@@ -23,8 +28,6 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap"
         />
-
-        
       </Head>
       <body>
         <Main />
